26cb481b3419cdbe81ebc49de7d7c43092ae4ebe
Brij Nandan Tripathi <52787434+brij0707@users.noreply.github.com>
2026-01-30 21:25:26 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/innomind/bestverifier/MainActivity.java
+++ android/app/src/main/java/com/innomind/bestverifier/MainActivity.java
@@ -1,4 +1,7 @@
 package com.innomind.bestverifier;
+
 import io.flutter.embedding.android.FlutterActivity;
+
 public class MainActivity extends FlutterActivity {
+    // No extra code needed here for standard v2 embedding
 }
